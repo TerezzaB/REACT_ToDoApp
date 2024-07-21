2eb152c340ef22029c26b78571915b4d209807df
Terézia Boďová <bodova.terezia13@gmail.com>
2024-07-22 01:11:58 +0200

    10th - homepage design issues

--- src/components/HomeComponents/HomeTyping.jsx
+++ src/components/HomeComponents/HomeTyping.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+
 export default function HomeTyping() {
     return (
-        <div className="typing-container p-40">
-            <h1 className="typing-effect">Welcome to my ToDo App...</h1>
+        <div className="m-10 text-center">
+            <h1 className="overflow-hidden border-r-4 border-white whitespace-nowrap text-xl sm:text-3xl md:text-5xl typing-effect">
+                Welcome to my ToDo App...
+            </h1>
         </div>
     )
 }
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -4,9 +4,9 @@ import HomeButton from "../components/HomeComponents/HomeButton";
 
 export default function HomeView() {
     return (
-        <>
+        <div className="flex flex-col gap-y-5 items-center justify-center min-h-screen">
             <HomeTyping />
             <HomeButton />
-        </>
+        </div>
     )
 }
